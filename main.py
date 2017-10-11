b857eae2080ce7caf879ce7133fbe6859cbd1ee1
kristinlin <klin11@stuy.edu>
2017-10-11 14:36:51 -0400

    classwork

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 #Work08 -- login 2.0
 #2017-10-08
 
-from flask import Flask, render_template, request, session, redirect, url_for
+from flask import Flask, render_template, request, session, redirect, url_for, flash
 import os
 
 form = Flask(__name__)
@@ -14,7 +14,7 @@ form.secret_key = os.urandom(8)
 @form.route('/', methods = ['POST', 'GET'])
 def root():
     if 'usr' in session:
-        return redirect(url_for('requests')) #welcome page
+        return requests() #go to welcome
     else:
         return render_template("root.html")
 
@@ -26,20 +26,19 @@ def requests():
     if 'usr' in session:
         return render_template("result.html", usr = session['usr'])
     #either POST or GET method
-    if request.method == 'POST':
-        usr = request.form["usr"]
-        pwd = request.form["pwd"]
-    else:
-        usr = request.args['usr']
-        pwd = request.args['pwd']
+    usr = request.form["usr"]
+    pwd = request.form["pwd"]
     #If username and password are correct, then results
     if usr == 'softdev' and pwd == 'pd09':
         session['usr'] = usr
         return render_template("result.html", usr = usr)
-    #else send to error page, with booleans for password and username
+    #flash error msgs
     else:
-        return render_template("error.html", cUsr = (usr == 'softdev'),
-        cPwd = (pwd == 'pd09'))
+        if not usr == 'softdev' :
+            flash('Invalid username')
+        if not pwd == 'pd09' :
+            flash('Incorrect password')
+        return redirect(url_for('root'))
 
 #logout route
 @form.route('/logout', methods = ['POST', 'GET'])
